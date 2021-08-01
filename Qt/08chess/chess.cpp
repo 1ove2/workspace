24f90035206343bcfa594ec9ebc05a93b9d97cae
sls <86581942@qq.com>
2021-08-01 19:23:49 +0800

    add chess

---
+++ Qt/08chess/chess.cpp
@@ -0,0 +1,60 @@
+#include "chess.h"
+
+chess::chess(QWidget *parent)
+    : QWidget(parent)
+{
+    initChessState();
+}
+
+chess::~chess()
+{
+}
+
+void chess::paintEvent(QPaintEvent *event){
+    QPainter painter(this);
+    QPen mypen;
+    mypen.setWidth(4);
+    mypen.setColor(Qt::black);
+    mypen.setStyle(Qt::SolidLine);
+    painter.setPen(mypen);
+    //画背景图
+    painter.drawPixmap(this->rect(),QPixmap(":/new/image/board.jpg"));
+    //画棋盘
+    for(int i = 0; i <= 8; i++){
+        painter.drawLine(start_x, start_y+i*this->grid_h,start_x+8*this->grid_w, start_y+i*this->grid_h);
+
+        painter.drawLine(start_x+i*this->grid_w,start_y,start_x+i*this->grid_w,start_y+8*this->grid_h);
+    }
+    //画棋子
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(this->chessState[i][j] == this->White){
+                painter.drawPixmap(start_x+grid_w*i,start_y+grid_h*j,grid_w,grid_h,QPixmap(":/new/image/white.png"));
+            }
+            else if(this->chessState[i][j] == this->Black){
+                painter.drawPixmap(start_x+grid_w*i,start_y+grid_h*j,grid_w,grid_h,QPixmap(":/new/image/black.png"));
+            }
+        }
+    }
+}
+
+void chess::resizeEvent(QResizeEvent *event){
+
+    this->grid_w = this->width()/10;
+    this->grid_h = this->height()/10;
+    this->start_x = this->grid_w;
+    this->start_y = this->grid_h;
+}
+
+void chess::initChessState(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            this->chessState[i][j] = this->Empty;
+        }
+    }
+    // 将棋盘中间的4个空间分别标志位2黑子2白子
+    this->chessState[3][3] = this->Black;
+    this->chessState[3][4] = this->White;
+    this->chessState[4][3] = this->White;
+    this->chessState[4][4] = this->Black;
+}
---
+++ Qt/08chess/chess.h
@@ -0,0 +1,34 @@
+#ifndef CHESS_H
+#define CHESS_H
+
+#include <QWidget>
+#include <QPainter>
+#include <QPen>
+#include <QPaintEvent>
+#include <QDebug>
+class chess : public QWidget
+{
+    Q_OBJECT
+
+public:
+    chess(QWidget *parent = nullptr);
+    ~chess();
+
+    enum GridState{Empty, Black, White};
+    void initChessState();
+
+protected:
+    //绘图事件,画棋盘棋子
+    void paintEvent(QPaintEvent *event);
+    //resize事件,自动调整界面棋子棋盘
+    void resizeEvent(QResizeEvent *event);
+
+private:
+    //棋子宽度高度
+    int grid_w ,grid_h;
+    //棋盘起点坐标
+    int start_x, start_y;
+    int chessState[8][8];
+};
+
+#endif // CHESS_H
