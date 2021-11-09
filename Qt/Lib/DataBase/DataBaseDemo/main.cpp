2777038e9953cb24452d712e7438da0dd317fabe
shils <86581942@qq.com>
2021-11-09 16:57:45 +0800

    add gitignore

--- Qt/Lib/DataBase/DataBaseDemo/main.cpp
+++ Qt/Lib/DataBase/DataBaseDemo/main.cpp
@@ -1,9 +1,12 @@
 #include "MainWindow.h"
 #include <QApplication>
-
+#include "LoginDlg.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //设置应用程序字体
+    a.setFont(QFont("Microsoft Yahei", 9));
+
     MainWindow w;
     w.show();
 
